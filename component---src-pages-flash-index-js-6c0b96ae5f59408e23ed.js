5bc582f5304901ae2d7a4c3935cc3d883d83f3c0
Peja <peja@relab.cc>
2022-06-09 17:03:57 +0800

    Updates

---
+++ component---src-pages-flash-index-js-6c0b96ae5f59408e23ed.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstyled_gatsby_starter=self.webpackChunkstyled_gatsby_starter||[]).push([[376],{9514:function(e,t,s){s.r(t);var a=s(7294),n=s(5485),r=s(4910);t.default=(0,n.Z)((function(e){return a.createElement(r.Z,Object.assign({name:"flash"},e))}))}}]);
+//# sourceMappingURL=component---src-pages-flash-index-js-6c0b96ae5f59408e23ed.js.map
