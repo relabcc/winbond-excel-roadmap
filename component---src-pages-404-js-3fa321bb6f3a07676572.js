5aec590882d75b6bc96224555a5f6314c018bd9c
Peja <peja@relab.cc>
2022-06-10 19:28:06 +0800

    Updates

--- component---src-pages-404-js-3fa321bb6f3a07676572.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkstyled_gatsby_starter=self.webpackChunkstyled_gatsby_starter||[]).push([[883],{9616:function(e,t,s){s.r(t);var n=s(7294);t.default=function(){return n.createElement("div",null,n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-3fa321bb6f3a07676572.js.map
